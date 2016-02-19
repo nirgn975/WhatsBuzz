25eb62c29ad24dd17e5e661dd3597ee393cc982f
nirgn975 <nirgn975@gmail.com>
2016-02-19 23:18:53 +0200

    add SASS integration to gulp

--- gulpfile.js
+++ gulpfile.js
@@ -2,20 +2,21 @@
 
 var gulp        = require('gulp');
 var browserSync = require('browser-sync').create();
-var sass = require('gulp-sass');
+var sass        = require('gulp-sass');
 
 /**
  * Convert SASS to CSS, minify all the files and add prefix.
  */
 gulp.task('sass', function () {
-  return gulp.src('./whats_buzz/static/sass/*.scss')
+  return gulp.src('./whats_buzz/static/sass/main.scss')
     //.pipe(sass().on('error', sass.logError))
     .pipe(sass({
       includePaths: ['css'],
       onError: browserSync.notify
     }))
     //.pipe(prefix(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true }))
-    .pipe(gulp.dest('./whats_buzz/static/css'));
+    .pipe(gulp.dest('./whats_buzz/static/css'))
+    .pipe(browserSync.reload({stream:true}));
 });
 
 /**
@@ -34,11 +35,11 @@ gulp.task('browser-sync', ['sass'], function() {
 gulp.task('watch', function () {
     gulp.watch('./whats_buzz/templates/**', browserSync.reload);
     gulp.watch('./whats_buzz/static/javascript/*.js', browserSync.reload);
-    gulp.watch('./whats_buzz/static/sass/*.scss', ['sass']);
+    gulp.watch('./whats_buzz/static/sass/**', ['sass']);
 });
 
 /**
  * Default task, running just `gulp` will compile the sass,
  * compile the jekyll site, launch BrowserSync & watch files.
  */
-gulp.task('default', ['sass', 'browser-sync', 'watch']);
+gulp.task('default', ['browser-sync', 'watch']);
