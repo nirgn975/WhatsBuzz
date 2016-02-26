8e69260fcbce206df4d23303259e9a275c805dfa
nirgn975 <nir@galons.io>
2016-02-27 00:33:53 +0200

    Add collapsable Facebook fields.

--- whats_buzz/admin.py
+++ whats_buzz/admin.py
@@ -3,16 +3,31 @@ from django.contrib import admin
 # Import my models.
 from whats_buzz.models import Post
 
+
 # Set up automated slug creation
 class PostAdmin(admin.ModelAdmin):
     model = Post
-    list_display = (
-        'name',
-        'description',
+    fieldsets = (
+        (None, {
+            'fields': ('name', 'description', 'slug', 'post_type', 'image_banner', 'buzz')
+        }),
+        ('Facebook - Profile', {
+            'classes': ('collapse',),
+            'fields': (),
+        }),
+        ('Facebook - Friends', {
+            'classes': ('collapse',),
+            'fields': (),
+        }),
+        ('Facebook - Likes', {
+            'classes': ('collapse',),
+            'fields': (),
+        }),
     )
     prepopulated_fields = {
         'slug': ('name',)
     }
+
 
 # And register them.as
 admin.site.register(Post, PostAdmin)
