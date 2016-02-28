65a1f48cfe88ef57bc5c25face0d094f99e2f88a
nirgn975 <nir@galons.io>
2016-02-29 00:40:10 +0200

    Email working.

--- whats_buzz/forms.py
+++ whats_buzz/forms.py
@@ -3,4 +3,5 @@ from django import forms
 
 class ContactForm(forms.Form):
     contact_name = forms.CharField()
+    content_checkbox = forms.BooleanField()
     content = forms.CharField(widget=forms.Textarea)
