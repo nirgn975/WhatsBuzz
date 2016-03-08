c3822d61fe406f2d9fa5a5865485ca0e9db7fa83
nirgn975 <nirgn975@gmail.com>
2016-03-08 20:40:01 +0200

    Finish "more posts" button.

--- whats_buzz/views/posts.py
+++ whats_buzz/views/posts.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 from whats_buzz.models import Post
 
-# Create your views here.
+
 def post_detail(request, slug):
+    '''
+    Get the post details by the slug.
+    '''
     post = Post.objects.get(slug=slug)
-    return render(request, 'pages/post.html', {'post': post})
+    random_posts = Post.objects.all().order_by('?')[:5]
+    return render(request, 'pages/post.html', {
+        'post': post,
+        'random_posts': random_posts
+    })
