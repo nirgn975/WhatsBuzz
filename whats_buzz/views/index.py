ee2de2208ed3880dc99273dc07b22895536a51d1
nirgn975 <nir@galons.io>
2016-03-13 21:28:16 +0200

    Add x & y for facebook profile, check profile name.

--- whats_buzz/views/index.py
+++ whats_buzz/views/index.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
 from whats_buzz.models import Post
 
-# Create your views here.
+
 def index(request):
+    """
+    Get the 5 first posts from 'FG' and 'TY', sorted by date (last ones).
+    Args:
+        request:
+
+    Returns:
+
+    """
     facebook_games_posts = Post.objects.filter(post_type='FG')[:5]
     test_yourself_posts = Post.objects.filter(post_type='TY')[:5]
     buzz_posts = Post.objects.filter(buzz=True)
@@ -13,7 +21,16 @@ def index(request):
         'buzz_posts': buzz_posts,
                                           })
 
+
 def get_all_posts_by_type(request):
+    """
+    Return all the posts by it's type (get it from the URL).
+    Args:
+        request:
+
+    Returns:
+
+    """
     url_path = request.path.replace('/', '')
     buzz_posts = Post.objects.filter(buzz=True)
     if url_path == "facebook-games":
--- whats_buzz/views/posts.py
+++ whats_buzz/views/posts.py
@@ -1,15 +1,29 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 from whats_buzz.models import Post
-
+from whats_buzz.models import User_Name_Game
 
 def post_detail(request, slug):
-    '''
-    Get the post details by the slug.
-    '''
+    """
+    Get the post detail by the slug.
+    Args:
+        request:
+        slug:
+
+    Returns:
+
+    """
     post = Post.objects.get(slug=slug)
     random_posts = Post.objects.all().order_by('?')[:5]
+
+    # Get the user name for facebook API.
+    try:
+        facebook_user_name = User_Name_Game.objects.get(post=post)
+    except User_Name_Game.DoesNotExist:
+        facebook_user_name = None
+
     return render(request, 'pages/post.html', {
         'post': post,
-        'random_posts': random_posts
+        'random_posts': random_posts,
+        'facebook_user_name': facebook_user_name,
     })
