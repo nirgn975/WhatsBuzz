be638cab9d7797f5159c6673552b81b490427b8c
Nir Galon <nirgn975@gmail.com>
2016-05-20 20:30:45 +0300

    Check fb share.

--- web/static/javascript/partials/app.js
+++ web/static/javascript/partials/app.js
@@ -26,16 +26,22 @@ function shareThisPost(){
   var image = url + $('#FB-image-game').attr('src');
   console.log(image);
 
-  // FB.ui({
-  //   method: 'share',
-  //   name: 'Something',
-  //   picture: image,
-  //   href: url,
-  //   link: url,
-  //   description: captionText,
-  //   caption: 'http://www.whatsbuzz.co.il'
-  //
-  // }, function(response){});
+  FB.ui({
+    method: 'share',
+    name: 'Something',
+    picture: image,
+    // href: url,
+    // link: url,
+    // description: captionText,
+    // caption: 'http://www.whatsbuzz.co.il'
+
+  }, function(response){
+    if (response && response.post_id) {
+      console.log('share success');
+    } else {
+      console.log('share error');
+    }
+  });
 }
 
 /**
