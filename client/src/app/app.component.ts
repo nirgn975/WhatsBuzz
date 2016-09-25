fd3815496609e67eedbfdafbeba2acd95a1930ed
Nir Galon <nirgn975@gmail.com>
2016-09-25 09:24:49 +0300

    Use the service and populate the title variable.

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { AppService } from './shared/app.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app works!';
+
+  constructor(
+    private appService: AppService
+  ) { }
+
+  ngOnInit() {
+    this.appService.getDummyData().subscribe(
+      (res) => this.title = res
+    );
+  }
 }
