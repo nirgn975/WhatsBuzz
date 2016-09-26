99095f3081384cad005019db3b6e74131a9cd5db
Nir Galon <nirgn975@gmail.com>
2016-09-26 13:31:02 +0300

    Add a new endpoint for translatable entity, to check internationalized API.

--- server/api/urls.py
+++ server/api/urls.py
@@ -4,6 +4,7 @@ from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
 router.register(r'users', views.UserViewSet, base_name='user')
+router.register(r'facebook-users', views.FacebookUserViewSet)
 
 urlpatterns = [
     url(r'^', include(router.urls)),
