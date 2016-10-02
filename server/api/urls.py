66c96cf5596d3249af1fcb19086438c98f833f00
Nir Galon <nirgn975@gmail.com>
2016-10-02 17:15:42 +0300

    Cleanup models and admin. (#76)
    * Fix license badge.
    
    * Fix all models, admin panel, and add translation to relevant fields.
    
    * Fix line length limit.

--- server/api/urls.py
+++ server/api/urls.py
@@ -1,10 +1,10 @@
-from api import views
+# from api import views
 from django.conf.urls import url, include
 from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
-router.register(r'users', views.UserViewSet, base_name='user')
-router.register(r'facebook-users', views.FacebookUserViewSet)
+# router.register(r'users', views.UserViewSet, base_name='user')
+# router.register(r'facebook-users', views.FacebookUserViewSet)
 
 urlpatterns = [
     url(r'^', include(router.urls)),
--- server/api/views.py
+++ server/api/views.py
@@ -1,21 +1,21 @@
 from rest_framework import viewsets
 from django.contrib.auth.models import User
-from api.serializers import UserSerializer, FacebookUserSerializer
+# from api.serializers import UserSerializer, FacebookUserSerializer
 
-from whatsbuzz.models import FacebookUser
-
-
-class UserViewSet(viewsets.ReadOnlyModelViewSet):
-    """
-    API endpoint that allows users to be viewed or edited.
-    """
-    queryset = User.objects.all().order_by('-date_joined')
-    serializer_class = UserSerializer
+# from whatsbuzz.models import FacebookUser
 
 
-class FacebookUserViewSet(viewsets.ReadOnlyModelViewSet):
-    """
-    API endpoint that allows Facebook users to be viewed or edited.
-    """
-    queryset = FacebookUser.objects.all()
-    serializer_class = FacebookUserSerializer
+# class UserViewSet(viewsets.ReadOnlyModelViewSet):
+#     """
+#     API endpoint that allows users to be viewed or edited.
+#     """
+#     queryset = User.objects.all().order_by('-date_joined')
+#     serializer_class = UserSerializer
+#
+#
+# class FacebookUserViewSet(viewsets.ReadOnlyModelViewSet):
+#     """
+#     API endpoint that allows Facebook users to be viewed or edited.
+#     """
+#     queryset = FacebookUser.objects.all()
+#     serializer_class = FacebookUserSerializer
