66af6fb62b0256463005b5e787f6ba6c1e6a4f6e
Nir Galon <nirgn975@gmail.com>
2016-10-03 10:27:20 +0300

    Add translations strings.

--- server/whatsbuzz/models.py
+++ server/whatsbuzz/models.py
@@ -25,13 +25,13 @@ class Post(models.Model):
     """
     The basic fields for every post.
     """
-    title = models.CharField(max_length=255, blank=True)
-    body = RichTextField(blank=True)
+    title = models.CharField(_('title'), max_length=255, blank=True)
+    body = RichTextField(_('body'), blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
-    banner_image = models.ImageField(upload_to='%Y/%m/%d/', blank=True)
-    buzz = models.BooleanField(default=False)
-    age_categories = models.CharField(max_length=25, choices=AgeCategories.choices, default='default')
-    publish = models.BooleanField(default=False)
+    banner_image = models.ImageField(_('banner image'), upload_to='%Y/%m/%d/', blank=True)
+    buzz = models.BooleanField(_('buzz'), default=False)
+    age_categories = models.CharField(_('age categories'), max_length=25, choices=AgeCategories.choices, default='default')
+    publish = models.BooleanField(_('publish'), default=False)
     REQUIRED_FIELDS = ['title', 'body', 'banner_image', ]
 
     class Meta:
@@ -42,19 +42,29 @@ class Trend(Post):
     """
     An article like post.
     """
+    class Meta:
+        verbose_name = _('Trend')
+        verbose_name_plural = _('Trends')
 
 
 class TestYourself(Post):
     """
     A quiz like post. The code should contain the quiz embedded code itself from playbuzz and alikes.
     """
-    code = models.TextField()
+    code = models.TextField(_('code'))
+
+    class Meta:
+        verbose_name = _('Test Yourself')
+        verbose_name_plural = _('Tests Yourself')
 
 
 class FacebookGame(Post):
     """
     Facebook Game contains all the facebook related entities.
     """
+    class Meta:
+        verbose_name = _('Facebook Game')
+        verbose_name_plural = _('Facebook Games')
 
 
 class FacebookGamesImage(models.Model):
@@ -62,7 +72,11 @@ class FacebookGamesImage(models.Model):
     All the background images for a Facebook Game.
     """
     post = models.ForeignKey(FacebookGame, related_name='background_image')
-    background_image = models.ImageField(upload_to='%Y/%m/%d/', blank=True)
+    background_image = models.ImageField(_('background image'), upload_to='%Y/%m/%d/', blank=True)
+
+    class Meta:
+        verbose_name = _('facebook background image')
+        verbose_name_plural = _('facebook background images')
 
 
 class FacebookUsername(models.Model):
@@ -70,11 +84,15 @@ class FacebookUsername(models.Model):
     The username options for a Facebook Game.
     """
     post = models.ForeignKey(FacebookGame, related_name='facebook_username')
-    username = models.CharField(max_length=255, choices=FacebookUserName.choices, default='empty')
-    x = models.PositiveIntegerField()
-    y = models.PositiveIntegerField()
-    color = models.CharField(max_length=255)
-    font_size = models.PositiveIntegerField()
+    username = models.CharField(_('username'), max_length=255, choices=FacebookUserName.choices, default='empty')
+    x = models.PositiveIntegerField(_('x'))
+    y = models.PositiveIntegerField(_('y'))
+    color = models.CharField(_('color'), max_length=255)
+    font_size = models.PositiveIntegerField(_('font_size'))
+
+    class Meta:
+        verbose_name = _('facebook username')
+        verbose_name_plural = _('facebook usernames')
 
 
 class FacebookProfileImage(models.Model):
@@ -82,10 +100,14 @@ class FacebookProfileImage(models.Model):
     The facebook profile image of the user, for a Facebook Game.
     """
     post = models.ForeignKey(FacebookGame, related_name='facebook_profile_image')
-    width = models.PositiveIntegerField()
-    height = models.PositiveIntegerField()
-    x = models.PositiveIntegerField()
-    y = models.PositiveIntegerField()
+    width = models.PositiveIntegerField(_('width'))
+    height = models.PositiveIntegerField(_('height'))
+    x = models.PositiveIntegerField(_('x'))
+    y = models.PositiveIntegerField(_('y'))
+
+    class Meta:
+        verbose_name = _('facebook profile image')
+        verbose_name_plural = _('facebook profile images')
 
 
 class User(models.Model):
@@ -93,7 +115,11 @@ class User(models.Model):
     All the facebook users that ever logged in to the site.
     """
     token = models.TextField()
-    email = models.CharField(max_length=225)
-    name = models.CharField(max_length=225)
+    email = models.CharField(_('email'), max_length=225)
+    name = models.CharField(_('name'), max_length=225)
     created_at = models.DateTimeField(auto_now_add=True)
-    last_time_visit = models.DateTimeField()
+    last_time_visit = models.DateTimeField(_('last_time_visit'))
+
+    class Meta:
+        verbose_name = _('Facebook User')
+        verbose_name_plural = _('Facebook Users')
