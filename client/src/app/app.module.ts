b285ac22c54424ffaba3108aa2aa4b3607c3478f
Nir Galon <nirgn975@gmail.com>
2016-10-04 09:31:32 +0300

    Add Redux dependency

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,7 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { platformBrowserDynamic } from "@angular/platform-browser-dynamic" ;
+import { NgModule, Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { createStore, Store, StoreEnhancer } from 'redux';
 
 import { AppComponent } from './app.component';
 import { AppService } from './shared/app.service';
