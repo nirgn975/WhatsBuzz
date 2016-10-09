2f65332a2a95d987ce4a93d42807eaabcd099ad9
Nir Galon <nirgn975@gmail.com>
2016-10-09 08:42:19 +0300

    Add simple counter.

---
+++ client/src/app/counter-action-creators.ts
@@ -0,0 +1,11 @@
+import { Action, ActionCreator } from 'redux';
+
+export const INCREMENT: string = 'INCREMENT';
+export const increment: ActionCreator<Action> = () => ({
+  type: INCREMENT
+});
+
+export const DECREMENT: string = 'DECREMENT';
+export const decrement: ActionCreator<Action> = () => ({
+  type: DECREMENT
+});
