38237bf86684ed719c1dbd8048c98858668934dc
Nir Galon <nirgn975@gmail.com>
2016-10-15 22:59:29 +0300

    Add Redux (#83)
    * Refactor to wb, and create models.
    
    * Finish Trend redux.
    
    * Fix tests.

---
+++ client/src/app/actions/index.ts
@@ -0,0 +1,11 @@
+import * as TrendActions from './TrendActions';
+
+// export here for object imports
+export {
+  TrendActions
+};
+
+// export here for injecting the dependencies (e.g. at bootstrap)
+export default [
+  TrendActions
+];
---
+++ client/src/app/models/facebookGame.ts
@@ -0,0 +1,8 @@
+export interface FacebookGame {
+  id: string;
+  title: string;
+  body: string
+  banner: string;
+  buzz: boolean;
+  age_category: string;
+}
