4a3aa7e594a1148787ec68c98de66bebe9029fb5
Nir Galon <nirgn975@gmail.com>
2016-10-16 18:44:22 +0300

    Cleanup, and add assets.

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,27 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { platformBrowserDynamic } from "@angular/platform-browser-dynamic" ;
-import { NgModule, Component } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import { createStore, Store, compose, StoreEnhancer } from 'redux';
-import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 import { AppStore } from './app-store';
-import { AppState, default as reducer } from './reducers';
 
 import { WhatsBuzzComponent } from './wb.component';
-import { AppService } from './shared/app.service';
 import { HeaderComponent } from './components';
 import { FooterComponent } from './components';
-
-let devtools: StoreEnhancer<AppState> =
-  window['devToolsExtension'] ?
-  window['devToolsExtension']() : f => f;
-
-let store: Store<AppState> = createStore<AppState>(
-  reducer,
-  compose(devtools)
-);
 
 @NgModule({
   declarations: [
@@ -34,14 +20,7 @@ let store: Store<AppState> = createStore<AppState>(
     FormsModule,
     HttpModule
   ],
-  providers: [
-    AppService,
-    { provide: AppStore , useValue: store }
-    // provide(AppStore, { useValue: store })
-  ],
+  providers: [],
   bootstrap: [WhatsBuzzComponent]
 })
 export class AppModule { }
-
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
