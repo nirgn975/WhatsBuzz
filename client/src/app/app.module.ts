21ab3991286704abe4a10ca927b540c08ada3bfa
Nir Galon <nir@galons.io>
2016-10-18 01:00:53 +0300

    Create the buzz side component (#89)
    * Create buzz endpoint.
    
    * Create buzz side post.
    
    * Style the post text.
    
    * Cleanup
    
    * Cleanup
    
    * Remove publish field from buzz endpoint.

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -6,19 +6,25 @@ import { HttpModule } from '@angular/http';
 import { WhatsBuzzComponent } from './wb.component';
 import { HeaderComponent } from './components';
 import { FooterComponent } from './components';
+import { BuzzComponent } from './components';
+
+import { BuzzService } from './services';
 
 @NgModule({
   declarations: [
     WhatsBuzzComponent,
     HeaderComponent,
     FooterComponent,
+    BuzzComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule
   ],
-  providers: [],
+  providers: [
+    BuzzService
+  ],
   bootstrap: [WhatsBuzzComponent]
 })
 export class AppModule { }
--- server/api/urls.py
+++ server/api/urls.py
@@ -6,6 +6,7 @@ router = DefaultRouter()
 router.register(r'trends', views.TrendViewSet)
 router.register(r'test-yourself', views.TestYourselfViewSet)
 router.register(r'facebook-games', views.FacebookGameViewSet)
+router.register(r'buzz', views.BuzzViewSet)
 
 urlpatterns = [
     url(r'^', include(router.urls)),
