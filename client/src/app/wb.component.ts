6f922de811962924da48d59620373fc785929f22
Nir Galon <nirgn975@gmail.com>
2016-10-20 00:07:32 +0300

    Add JIT translation with ng2-translate.

--- client/src/app/wb.component.ts
+++ client/src/app/wb.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { TranslateService } from 'ng2-translate/ng2-translate';
 
 @Component({
   selector: 'wb-root',
@@ -12,4 +13,14 @@ import { Component } from '@angular/core';
   `
 })
 export class WhatsBuzzComponent {
+
+  constructor(translate: TranslateService) {
+       // this language will be used as a fallback when a translation isn't
+       // found in the current language.
+       translate.setDefaultLang('en');
+
+        // the lang to use, if the lang isn't available, it will use the
+        // current loader to get them.
+       translate.use('he');
+   }
 }
