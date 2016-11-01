2a0e671d58fcea74d5672dd098d2cef3383a84e7
Nir Galon <nirgn975@gmail.com>
2016-11-01 22:06:57 +0200

    Get the buzz posts from buzzState

--- client/src/app/reducers/buzz.ts
+++ client/src/app/reducers/buzz.ts
@@ -3,20 +3,20 @@ import { Action } from '@ngrx/store';
 import { Buzz } from '../models';
 import { BuzzActions } from '../actions';
 
-export type BuzzState = Buzz;
+export type BuzzState = Buzz[];
 
-const initialState: BuzzState = {
+const initialState: BuzzState = [{
   unique_id: '',
   title: '',
   banner_image: ''
-};
+}];
 
 export default function (state = initialState, action: Action): BuzzState {
   switch (action.type) {
     case BuzzActions.LOAD_BUZZS: {
       return initialState;
     }
-    case BuzzActions.GET_BUZZ_SUCCESS: {
+    case BuzzActions.LOAD_BUZZS_SUCCESS: {
       return action.payload;
     }
     default: {
