4d4206f7d05f1926fc46b6188c95a1e14ecd6181
Nir Galon <nirgn975@gmail.com>
2016-11-04 21:45:57 +0200

    Add facebookGames and trends posts to the main page

--- client/src/app/components/post/post.component.ts
+++ client/src/app/components/post/post.component.ts
@@ -1,15 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'wb-post',
   templateUrl: './post.component.html',
   styleUrls: ['./post.component.scss']
 })
-export class PostComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
+export class PostComponent {
+  @Input() post;
 
 }
