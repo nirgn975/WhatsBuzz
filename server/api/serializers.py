3e6e1266168f4a46058fc50ad748d8c7572e6fb7
Nir Galon <nirgn975@gmail.com>
2016-11-07 07:48:58 +0200

    Cleanup (#117)
    * Cleanup styles.
    
    * Cleanup endpoint
    
    * Clean logs.

--- server/api/serializers.py
+++ server/api/serializers.py
@@ -1,6 +1,5 @@
 from rest_framework import serializers
-from whatsbuzz.models import Post, Tags, Trend, FacebookGame, FacebookGamesImage, FacebookUsername, \
-    FacebookProfileImage
+from whatsbuzz.models import Post, Tags, Trend, FacebookGame
 
 
 class TagsSerializer(serializers.HyperlinkedModelSerializer):
@@ -17,34 +16,12 @@ class TrendSerializer(serializers.HyperlinkedModelSerializer):
         fields = ('unique_id', 'title', 'body', 'banner_image', 'buzz', 'age_categories', 'tags')
 
 
-class FacebookGamesImageSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = FacebookGamesImage
-        fields = ('background_image', )
-
-
-class FacebookUsernameSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = FacebookUsername
-        fields = ('username', 'x', 'y', 'color', 'font_size')
-
-
-class FacebookProfileImageSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = FacebookProfileImage
-        fields = ('width', 'height', 'x', 'y')
-
-
 class FacebookGameSerializer(serializers.HyperlinkedModelSerializer):
-    background_image = FacebookGamesImageSerializer(many=True, read_only=True)
-    facebook_username = FacebookUsernameSerializer(many=True, read_only=True)
-    facebook_profile_image = FacebookProfileImageSerializer(many=True, read_only=True)
     tags = TagsSerializer(many=True, read_only=True)
 
     class Meta:
         model = FacebookGame
-        fields = ('unique_id', 'title', 'body', 'banner_image', 'buzz', 'age_categories', 'background_image',
-                  'facebook_username', 'facebook_profile_image', 'tags')
+        fields = ('unique_id', 'title', 'body', 'banner_image', 'buzz', 'age_categories', 'tags')
 
 
 class BuzzSerializer(serializers.HyperlinkedModelSerializer):
