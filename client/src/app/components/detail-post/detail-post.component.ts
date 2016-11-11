ca2729dae3c3a2505b0c1baf53874f41f312f661
Nir Galon <nir@galon.io>
2016-11-11 17:38:01 +0200

    Add facebook comments, user token, and finish style the detail post page

--- client/src/app/components/detail-post/detail-post.component.ts
+++ client/src/app/components/detail-post/detail-post.component.ts
@@ -7,6 +7,8 @@ import { AppState } from '../../reducers';
 import { DetailPostActions } from '../../actions';
 import { DetailPost } from '../../models';
 
+declare const FB:any;
+
 @Component({
   selector: 'wb-detail-post',
   templateUrl: './detail-post.component.html',
@@ -23,11 +25,38 @@ export class DetailPostComponent implements OnInit {
     store.select(state => state.detailPost).subscribe(
       (res) => this.detailPost = res
     );
+
+    FB.init({
+      appId      : '1063610257017045',
+      cookie     : true,  // enable cookies to allow the server to access
+                          // the session
+      xfbml      : true,  // parse social plugins on this page
+      version    : 'v2.5' // use graph api version 2.5
+    });
   }
 
   ngOnInit() {
     let postId = this.route.snapshot.params['uuid'];
     this.store.dispatch(this.detailPostActions.loadDetailPost(postId));
+
+    FB.getLoginStatus(response => {
+      this.statusChangeCallback(response);
+    });
+  }
+
+  onFacebookLoginClick() {
+    FB.login();
+  }
+
+  statusChangeCallback(resp) {
+    if (resp.status === 'connected') {
+      console.log(resp);
+      // connect here with your server for facebook login by passing access token given by facebook
+    } else if (resp.status === 'not_authorized') {
+      console.log(resp);
+    } else {
+      console.log(resp);
+    }
   }
 
 }
