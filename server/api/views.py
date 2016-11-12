349d394d41c74e2cca3bfef5ae1094476df7fdfc
Nir Galon <nir@galon.io>
2016-11-12 17:21:53 +0200

    Cleanup imports.

--- server/api/views.py
+++ server/api/views.py
@@ -1,7 +1,5 @@
 from django.utils import timezone
-from rest_framework import viewsets, status
-
-from rest_framework.decorators import detail_route
+from rest_framework import viewsets
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
