d015fe723a0e7814b9d5ff47dd452fb993031ee4
Nir Galon <nir@galon.io>
2016-12-03 12:25:49 +0200

    Style the component, and edit the server local_settings file

--- client/src/app/components/not-found/not-found.component.ts
+++ client/src/app/components/not-found/not-found.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { Store } from '@ngrx/store';
+
+import { AppState } from '../../reducers';
+import { FacebookGamesActions, TrendsActions, PagesActions } from '../../actions';
+import { PrePost, Pages } from '../../models';
 
 @Component({
   selector: 'wb-not-found',
@@ -6,10 +12,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./not-found.component.scss']
 })
 export class NotFoundComponent implements OnInit {
+  private facebookGames: Observable<PrePost[]>;
+  private trends: Observable<PrePost[]>;
+  private pages: Pages;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(
+    private store: Store<AppState>,
+    private facebookGamesActions: FacebookGamesActions,
+    private trendsActions: TrendsActions,
+    private pagesActions: PagesActions,
+  ) {
+    this.facebookGames = store.select(state => state.facebookGames);
+    this.trends = store.select(state => state.trends);
+    store.select(state => state.pages).subscribe(
+      (res) => this.pages = res
+    );
   }
 
+  ngOnInit() {
+    if (this.pages.facebookGames == 1) {
+      // Only if we're on the first page.
+      this.store.dispatch(this.facebookGamesActions.loadPosts(this.pages.facebookGames));
+    }
+
+    if (this.pages.trends == 1) {
+      // Only if we're on the first page.
+      this.store.dispatch(this.trendsActions.loadPosts(this.pages.trends));
+    }
+  }
 }
