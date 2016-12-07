3af0216978a781324c6847f2bf8f84ba199ef2a9
Nir Galon <nir@galon.io>
2016-12-08 00:05:41 +0200

    Try refactor by ngrx example

--- client/src/app/reducers/buzz.ts
+++ client/src/app/reducers/buzz.ts
@@ -1,26 +1,65 @@
-import { Action } from '@ngrx/store';
+// import { Action } from '@ngrx/store';
+//
+// import { PrePost } from '../models';
+// import { BuzzActions } from '../actions';
+//
+// export type BuzzState = PrePost[];
+//
+// const initialState: BuzzState = [{
+//   unique_id: '',
+//   title: '',
+//   banner_image: ''
+// }];
+//
+// export default function (state = initialState, action: Action): BuzzState {
+//   switch (action.type) {
+//     case BuzzActions.LOAD_BUZZS: {
+//       return initialState;
+//     }
+//     case BuzzActions.LOAD_BUZZS_SUCCESS: {
+//       return action.payload;
+//     }
+//     default: {
+//       return state;
+//     }
+//   }
+// }
 
+import { createSelector } from 'reselect';
 import { PrePost } from '../models';
-import { BuzzActions } from '../actions';
+import * as buzz from '../actions/buzz';
 
-export type BuzzState = PrePost[];
 
-const initialState: BuzzState = [{
-  unique_id: '',
-  title: '',
-  banner_image: ''
-}];
+export interface State {
+  ids: string[];
+  entities: { [id: string]: PrePost };
+};
 
-export default function (state = initialState, action: Action): BuzzState {
+const initialState: State = {
+  ids: [],
+  entities: {},
+};
+
+export function reducer(state = initialState, action: buzz.Actions): State {
   switch (action.type) {
-    case BuzzActions.LOAD_BUZZS: {
+    case buzz.ActionTypes.LOAD_BUZZS: {
       return initialState;
     }
-    case BuzzActions.LOAD_BUZZS_SUCCESS: {
+
+    case buzz.ActionTypes.LOAD_BUZZS_SUCCESS: {
       return action.payload;
     }
+
     default: {
       return state;
     }
   }
 }
+
+export const getEntities = (state: State) => state.entities;
+
+export const getIds = (state: State) => state.ids;
+
+export const getAll = createSelector(getEntities, getIds, (entities, ids) => {
+  return ids.map(id => entities[id]);
+});
