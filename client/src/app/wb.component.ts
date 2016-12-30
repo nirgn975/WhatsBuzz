e77d833bd199b54b4478fbec1441ad94db7cf14a
Nir Galon <nir@galons.io>
2016-12-31 00:10:54 +0200

    Make custom share buttons, for detail-post (#200)
    * Share the post on Facebook
    
    * Fix Google Analytics
    
    * Cleanup

--- client/src/app/wb.component.ts
+++ client/src/app/wb.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 
 import { FacebookService, FacebookInitParams } from 'ng2-facebook-sdk/dist';
+declare var ga: any;
 
 @Component({
   selector: 'wb-root',
@@ -8,7 +10,20 @@ import { FacebookService, FacebookInitParams } from 'ng2-facebook-sdk/dist';
   styleUrls: ['./wb.component.scss']
 })
 export class WbComponent {
+  private currentRoute:string;
+
   constructor(
     private fb: FacebookService,
-  ) { }
+    private router: Router,
+  ) {
+    router.events.distinctUntilChanged((previous: any, current: any) => {
+      if(current instanceof NavigationEnd) {
+        return previous.url === current.url;
+      }
+      return true;
+    }).subscribe((x: any) => {
+      console.log('router.change', x);
+      ga('send', 'pageview', x.url);
+    });
+  }
 }
