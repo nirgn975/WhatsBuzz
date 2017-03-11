cfe44af3514b13151038958bf0daba7a78cb3067
nirgn975 <nir@galon.io>
2017-03-11 18:02:40 +0200

    Upgrade angular-cli

--- client/e2e/app.e2e-spec.ts
+++ client/e2e/app.e2e-spec.ts
@@ -1,14 +1,14 @@
 import { WhatsbuzzPage } from './app.po';
 
-describe('whatsbuzz App', function() {
+describe('whatsbuzz App', () => {
   let page: WhatsbuzzPage;
 
   beforeEach(() => {
     page = new WhatsbuzzPage();
   });
 
-  // it('should display message saying app works', () => {
-  //   page.navigateTo();
-  //   expect(page.getParagraphText()).toEqual('app works!');
-  // });
+  it('should display message saying app works', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('app works!');
+  });
 });
